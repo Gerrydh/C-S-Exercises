6716e181501183eee4c13798eb213628f33ee304
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-03-16 10:08:13 +0000

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,12 @@
 # Gerard Hanlon, 15.03.2018
 # A program that displays the Fibonacci numbers
 
-def fibonacci(n):
-    a,b = 1,1
+def fibonacci(n): # n returns the nth number
+    a,b = 1,1   # define the first two numbers in the sequence
     for i in range(n-1):
-        a,b = b, a+b
-    return a
+        a,b = b, a+b   # move a and b forward 1 unit within each loop. b t will be 1 after that which is a+b
+    return a # return the fibonacci number which is the current number
   
-x = int(input("Input a number to calculate the Fibonacci :"))
+x = int(input("Input a number to calculate the Fibonacci :")) # Enter number (x). This will calculate the given Fibonacci number
 ans = fibonacci(x)
 print("Fibonacci number", x, "is", ans)
