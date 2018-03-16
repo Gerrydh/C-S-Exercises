57b924b44194b8dac15b3e1c10a6d421c0edd685
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-03-16 13:50:58 +0000

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -7,6 +7,6 @@ def fibonacci(n): # n returns the nth number
         a,b = b, a+b   # move a and b forward 1 unit within each loop. b t will be 1 after that which is a+b
     return a # return the fibonacci number which is the current number
   
-x = int(input("Input a number to calculate the Fibonacci :")) # Enter number (x). This will calculate the given Fibonacci number
-ans = fibonacci(x)
+x = int(input("Input a number to calculate the Fibonacci :")) # Enter number (x). This will return the given Fibonacci number
+ans = fibonacci(x) # The answer is the result of the given integer (x)
 print("Fibonacci number", x, "is", ans)
