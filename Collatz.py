8dd0e17f7e06ef7693ff56dccb400b8237e75dc5
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-03-27 13:47:32 +0100

    Updated with additional comments

--- Collatz.py
+++ Collatz.py
@@ -1,14 +1,18 @@
 # Gerard Hanlon, 2018-02-08
-# Collatz Exercise
+# Collatz Exercise. If the integer is odd, then multiply by 3 and add 1. If
+# the integer is even, then devide by 2. The integer should eventually return 
+# the integer 1. 
 
+# n = any integer entered.
 n = int(input("Please enter an Integer:"))
 
+# as long as n is a psoitive greater than 1
 while n > 1:
-  if n % 2 == 0:
-      n = (n // 2)
-      print(n)
-  elif n % 1 == 0:
-      n = (n * 3 + 1)
-      print(n)
-  else:
-     print(n)
+  if n % 2 == 0: # if the integer is even
+      n = (n // 2) # devide by 2
+      print(n) # print if the integer is 1
+  elif n % 1 == 0: # or else, if the integer is odd
+      n = (n * 3 + 1) # then multiply the integer by 3 and add 1
+      print(n) # print if the integer is 1
+  else: # or else
+     print(n) # print if the integer is 1
