977c5322b81d621687d9fe14e043a5c592991162
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-03-27 15:27:18 +0100

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,3 +1,7 @@
+# Gerard Hanlon. 28.02.2018
+# Opening and formatting the Iris Data file
+
+# open the data file as f
 with open("data/iris.csv") as f:
-    for line in f:
+    for line in f: 
       print(line.split(',') [0:4])
