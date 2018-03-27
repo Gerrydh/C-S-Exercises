7d011bce725921dc6afd3965067bd05620dc9d28
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-03-27 16:09:08 +0100

    Updated with script with additional comments

--- iris.py
+++ iris.py
@@ -1,7 +1,7 @@
 # Gerard Hanlon. 28.02.2018
 # Opening and formatting the Iris Data file
 
-# open the data file as f
+# open the data file and give it the variable name f and then close the file when complete
 with open("data/iris.csv") as f:
-    for line in f: 
+    for line in f: # read the file line by line
       print(line.split(',') [0:4])
