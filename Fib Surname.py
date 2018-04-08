b4b8e6c4142e8f0c69910f5e9dd353e706671618
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-04-08 20:55:56 +0100

    Update Fib Surname.py
    Updated script with comments

--- Fib Surname.py
+++ Fib Surname.py
@@ -1,27 +1,27 @@
-# Gerard Hanlon
+# Gerard Hanlon, 30.01.2018
 # A program that displays Fibonacci numbers.
 
 def fib(n):
   """This function returns the nth Fibonacci numbers."""
-  i = 0
-  j = 1
-  n = n - 1
+  i = 0 # variable i = the first fibonacci number
+  j = 1 # variable j = the second fibonacci number
+  n = n - 1 # variable n = n - 1
 
-  while n >= 0:
-    i, j = j, i + j
-    n = n - 1
+  while n >= 0: # while n is greater than 0
+    i, j = j, i + j # 0, 1 = 1, 0 + 1
+    n = n - 1 # we want the script to add the number preceeding it
   
-  return i
+  return i # return the new value of i
 
-name = "Hanlon"
-first = name[0]
-last = name [-1]
-firstno = ord (first)
-lastno = ord(last)
-x = firstno + lastno
+name = "Hanlon" # My surname
+first = name[0] # The first letter of my Surname- H
+last = name [-1] # The last letter of my surname- N
+firstno = ord (first) # The fibonacci number for 8- H is the 8th letter in the alphabet
+lastno = ord(last) # The fibonacci for number 14- N is the 14th letter in the alphabet
+x = firstno + lastno # x = the final fibonacci number we are looking for- The fibonacci numbers of the first and last letters of my surname added together
 
-ans = fib(x)
-print("My surname is", name)
-print("The first letter", first, "is number", firstno)
-print("The last letter", last, "is number", lastno)
-print("Fibonacci number", x, "is", ans) 
+ans = fib(x) # ans = the fibonacci of x
+print("My surname is", name) # prints my surname
+print("The first letter", first, "is number", firstno) # returns the fibonacci of the first letter of my surname
+print("The last letter", last, "is number", lastno) # returns the fibonacci number of the last letter of my surname
+print("Fibonacci number", x, "is", ans) # x = the total fibonacci number
