a12aa47350b63e3c05a93709900f39d73d6a84d4
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-04-08 20:03:29 +0100

    Update Factorial.py
    Updated comments

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,6 @@
 # Gerard Hanlon, 12.03.2018
 # The factorial of a number is the number multiplied by all of the positive numbers less than it
-# 3 points to consider, the integer must be positive, and it must be greater than 1
+# 2 points to consider, the integer must be positive, and it must be greater than 1
 #https://www.w3resource.com/python-exercises/python-functions-exercise-5.php
 
 def factorial(n): # defining the factorial function n(the chosen integer)
